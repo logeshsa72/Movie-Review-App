816c3197ac00d3c343752e482d8c6f2077f8da45
logeshsa72 <logeshsmart72@gmail.com>
2025-12-07 23:40:26 +0530

    commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',   // ✅ IMPORTANT for Vercel / GitHub Pages / Static deploy
   plugins: [
     react(),
     tailwindcss(),
